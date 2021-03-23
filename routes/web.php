c7cdc9480ad2d856c79f471b63eb2f5b63af2a55
andrea <corro.andrea.1995@gmail.com>
2021-03-23 16:37:28 +0100

    creazione rotte diverse users e admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('index');
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->group(function () {
+Route::get('/', 'HomeController@index')->name('home');
+  });
